29d271978be2b58475f0b9fe04e09d1aad412d26
Helpdesk IT <helpdesk.it@apotheconpharma.com>
2026-06-03 12:20:28 +0530

    ads add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,29 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import FrontPage from "./FrontPage";
 
+// loader function for ads.txt
+async function adsTxtLoader() {
+  const text = `google.com, pub-9385539987256182, DIRECT, f08c47fec0942fa0`;
+  return new Response(text, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
 
 function App() {
-
   return (
     <div className="App">
-    
       <Routes>
-      
-        <Route
-          path="/"
-          element={
-              <FrontPage/>
-          }
+        <Route path="/" element={<FrontPage />} />
+        <Route 
+          path="/ads.txt" 
+          loader={adsTxtLoader}
+          element={<div />}
         />
       </Routes>
-      
     </div>
   );
 }
-export default App;
 
+export default App;
